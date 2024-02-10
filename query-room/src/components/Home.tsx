1c693200fe7e0764bc6e6c04cadc8fcf1ad51b55
Iwasaki-2310 <iwachan2310@gmail.com>
2024-02-10 20:14:57 +0900

    ログイン状態を識別

--- query-room/src/components/Home.tsx
+++ query-room/src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { signInWithPopup, signOut } from 'firebase/auth'
+import { signInWithPopup } from 'firebase/auth'
 import { auth, provider } from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
