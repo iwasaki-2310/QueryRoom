b1804fa2b0c10b9889f83941cfda42458a11a1cc
Iwasaki-2310 <iwachan2310@gmail.com>
2024-02-10 19:55:14 +0900

    アナリティクス削除

--- query-room/src/firebase.js
+++ query-room/src/firebase.js
@@ -1,23 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXTpEtEFQzXNXRS9s_vy1sABAhTRTuwvA",
-  authDomain: "queryroom-9178b.firebaseapp.com",
-  projectId: "queryroom-9178b",
-  storageBucket: "queryroom-9178b.appspot.com",
-  messagingSenderId: "127495684578",
-  appId: "1:127495684578:web:679fd1816c40d17b6139bb",
-  measurementId: "G-99CR8818KH"
-};
+  apiKey: 'AIzaSyBXTpEtEFQzXNXRS9s_vy1sABAhTRTuwvA',
+  authDomain: 'queryroom-9178b.firebaseapp.com',
+  projectId: 'queryroom-9178b',
+  storageBucket: 'queryroom-9178b.appspot.com',
+  messagingSenderId: '127495684578',
+  appId: '1:127495684578:web:679fd1816c40d17b6139bb',
+  measurementId: 'G-99CR8818KH',
+}
 
+const app = initializeApp(firebaseConfig)
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
 
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export {auth, provider}
+export { auth, provider }
