efe5aceff1c6ab117b5e09aa4a9e9316bacf052e
Iwasaki-2310 <iwachan2310@gmail.com>
2024-02-10 16:04:30 +0900

    一旦

--- query-room/src/App.tsx
+++ query-room/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>これは自動デプロイのテストです</h1>
+        <h1>これは自動デプロイのテストです。</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
