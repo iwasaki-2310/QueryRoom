5011371be74493e1819c560d59807ec40564b2a9
Iwasaki-2310 <iwachan2310@gmail.com>
2024-02-10 20:13:36 +0900

    ログイン状態を識別

--- query-room/src/components/Home.tsx
+++ query-room/src/components/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { signInWithPopup } from 'firebase/auth'
+import { signInWithPopup, signOut } from 'firebase/auth'
 import { auth, provider } from '../firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 const Home = () => {
+  const [user] = useAuthState(auth)
   return (
     <div>
+      {user ? (
+        <>
+          <UserInfo />
+          <SignOutButton />
+        </>
+      ) : (
+        <SignInButton />
+      )}
       <SignInButton />
     </div>
   )
@@ -19,4 +29,13 @@ const SignInButton = () => {
     signInWithPopup(auth, provider)
   }
   return <button onClick={signInWithGoogle}>Googleでサインイン</button>
+}
+
+// サインアウトするボタン
+const SignOutButton = () => {
+  return <button onClick={() => auth.signOut()}>サインアウト</button>
+}
+
+const UserInfo = () => {
+  return <>ユーザー情報</>
 }
