cebd50ea8ff931e437084b6efd638c368bcf3aab
Iwasaki-2310 <iwachan2310@gmail.com>
2024-02-28 23:17:04 +0900

    チャットルームのコンポーネント分割、CSS調整

--- query-room/src/components/molecules/ChatMessage.tsx
+++ query-room/src/components/molecules/ChatMessage.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import { roomData } from '../../types/roomData'
 import { UserIcon } from '../atom/UserIcon'
 import { Message } from '../atom/Message'
